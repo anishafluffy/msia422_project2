ecff74f08c455c89c21418e56c7516df818d8c29
Anisha Dubhashi <anisha.dubhashi@gmail.com>
2017-12-06 14:28:17 -0600

    add remainder of slice and sort

--- src/project2/MyTest.java
+++ src/project2/MyTest.java
@@ -5,28 +5,28 @@ import java.io.IOException;
 public class MyTest {
 
 	public static void main(String[] args) throws IOException {
-		//MyPandas class
-		//1. readCSV
+	//MyPandas class
+	//1. readCSV
 		MyDataFrame df1 = MyPandas.readCSV("./CA.txt");
 		MyDataFrame df2 = MyPandas.readCSV("./TX.txt");
 		
-		//2. writeCSV
+	//2. writeCSV
 		//MyPandas.writeCSV(df1, "./Output.txt");
 		//MyPandas.writeCSV(df, "./Combined.txt");
 		
-		//3. concat
+	//3. concat
 		MyDataFrame df = MyPandas.concat(df1, df2);
 		
-		//MyDataFrame class
-		//1. Head and Tail
+	//MyDataFrame class
+	//1. Head and Tail
 		//df.head(5);
 		//df.tail(5);
 		
-		//2. dType
+	//2. dType
 		//df.dType(2);
 		//df.dType("STATE");
 		
-		//3. Slicing
+	//3. Slicing
 		//df.slice(3); //gives an error when enter incorrect
 		//df.slice(5); //gives an error when enter incorrect
 		//df.slice("name"); //gives an error when enter incorrect
@@ -34,26 +34,26 @@ public class MyTest {
 		//int[] indexArr = {1,3,4};
 		//df.slice(indexArr);
 		
-		//4. Filtering
+		String[] nameArr = {"name", "count"};
+		df.slice(nameArr);
+		
+	//4. Filtering
 		
 		
-		//5. Indexing
+	//5. Indexing
 		//df.loc(700000);
 		//df.loc(5,10);	
 		
-		//6. Sorting
+	//6. Sorting
 		//df.sort(3);
+		//df.sort("count");
 		
-		//7. Aggregation
+	//7. Aggregation
 		//df.getMax(4);
 		//df.getMin(3);
 		//df.getMax("count");
 		//df.getMin("count");
 
-
-
-
-	
 	}
 
 }
